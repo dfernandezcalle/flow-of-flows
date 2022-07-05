ea2b156674e70dd5e3f31682447a238f467a0c7c
David Fernández Calle <david.fernandez@nextail.co>
2022-07-05 13:27:12 +0200

    update repo branch

--- flows/02_dbt.py
+++ flows/02_dbt.py
@@ -72,7 +72,7 @@ with Flow(FLOW_NAME, storage=STORAGE, run_config=LocalRun(labels=["dev"])) as fl
     dbt_repo = Parameter(
         "dbt_repo_url", default="https://github.com/dfernandezcalle/jaffle_shop"
     )
-    dbt_repo_branch = Parameter("dbt_repo_branch", default="core-v1.0.0")
+    dbt_repo_branch = Parameter("dbt_repo_branch", default=None)
     pull_task = pull_dbt_repo(dbt_repo, dbt_repo_branch)
     del_task.set_downstream(pull_task)
 
