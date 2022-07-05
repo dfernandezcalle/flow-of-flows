6f6e28d30e80a1502c47de44b189ea8a6fff4e8f
David Fernández Calle <david.fernandez@nextail.co>
2022-07-05 17:02:27 +0200

    rename flow and change requirements

--- flows/04_orchestrating_flow.py
+++ flows/04_orchestrating_flow.py
@@ -1,6 +1,6 @@
 from prefect import Flow
 from prefect.storage import GitHub
-from prefect.tasks.prefect import create_flow_run, wait_for_flow_run
+from prefect.tasks.prefect import StartFlowRun
 from prefect.run_configs import LocalRun
 
 FLOW_NAME = "04_orchestrating_flow"
@@ -10,35 +10,13 @@ STORAGE = GitHub(
     access_token_secret="GITHUB_ACCESS_TOKEN",
 )
 PROJECT_NAME = "jaffle_shop"
+start_flow_run = StartFlowRun(project_name=PROJECT_NAME, wait=True)
 
 
 with Flow(FLOW_NAME, storage=STORAGE, run_config=LocalRun(labels=["dev"])) as flow:
-    extract_load_id = create_flow_run(
-        flow_name="01_extract_load",
-        project_name=PROJECT_NAME,
-        task_args={"name": "Staging"},
-    )
-    extract_load_wait_task = wait_for_flow_run(
-        extract_load_id, raise_final_state=True, task_args={"name": "Staging - wait"}
-    )
+    staging = start_flow_run(flow_name="01_extract_load", task_args={"name": "Stage"})
+    dbt_run = start_flow_run(flow_name="02_dbt", task_args={"name": "DBT"})
+    dashboards = start_flow_run(flow_name="03_dashboards", task_args={"name": "Refresh"})
 
-    transform_id = create_flow_run(
-        flow_name="02_dbt",
-        project_name=PROJECT_NAME,
-        raise_final_state=True,
-        task_args={"name": "DBT flow"},
-    )
-    transform_id_wait_task = wait_for_flow_run(
-        transform_id, raise_final_state=True, task_args={"name": "DBT flow - wait"}
-    )
-    extract_load_wait_task.set_downstream(transform_id)
-
-    dashboards_id = create_flow_run(
-        flow_name="03_dashboards",
-        project_name=PROJECT_NAME,
-        task_args={"name": "Dashboards"},
-    )
-    dashboards_wait_task = wait_for_flow_run(
-        dashboards_id, raise_final_state=True, task_args={"name": "Dashboards - wait"}
-    )
-    transform_id_wait_task.set_downstream(dashboards_id)
+    staging.set_downstream(dbt_run)
+    dbt_run.set_downstream(dashboards)
