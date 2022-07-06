82fd3caf3841de4c8aa67792e60914af43c4fac4
David Fernández Calle <david.fernandez@nextail.co>
2022-07-06 15:44:05 +0200

    update db_utils

--- flow_utilities/db_utils.py
+++ flow_utilities/db_utils.py
@@ -66,7 +66,7 @@ def load_df_to_snowflake(df: pd.DataFrame, table_name: str, schema: str = "JAFFL
     conn_string = get_snowflake_connection_string()
     db_engine = create_engine(conn_string)
     conn = db_engine.connect()
-    conn.execute(f"USE DATABASE {database}")
+    conn.execute(f"USE DATABASE {database};")
     df.to_sql(table_name, schema=schema, con=db_engine, if_exists="replace", index=False)
     conn.close()
 
