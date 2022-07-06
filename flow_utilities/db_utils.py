54b51c9e064465ae3df0a36d9b6448619937b500
David Fernández Calle <david.fernandez@nextail.co>
2022-07-06 12:27:54 +0200

    rename flows & add new flow

--- flow_utilities/db_utils.py
+++ flow_utilities/db_utils.py
@@ -1,11 +1,16 @@
-"""
-Using local DB in docker:
-docker run -d --name demo_postgres -v dbdata:/var/lib/postgresql/data -p 5433:5432 -e POSTGRES_PASSWORD=xyz postgres:11
-"""
 import pandas as pd
 from prefect.client import Secret
 from sqlalchemy import create_engine
+from prefect import resource_manager
 
+## postgres
+"""
+Using local DB in docker:
+docker run -d --name demo_postgres -v dbdata:/var/lib/postgresql/data -p 5433:5432 -e POSTGRES_PASSWORD=xyz postgres:11
+
+export PREFECT__CONTEXT__SECRETS__POSTGRES_USER=postgres
+export PREFECT__CONTEXT__SECRETS__POSTGRES_PASS=your_password_1234
+"""
 
 def get_db_connection_string() -> str:
     user = Secret("POSTGRES_USER").get()
@@ -28,3 +33,53 @@ def load_df_to_db(df: pd.DataFrame, table_name: str, schema: str = "jaffle_shop"
     conn.execute(f"DROP TABLE IF EXISTS {schema}.{table_name} CASCADE;")
     df.to_sql(table_name, schema=schema, con=db_engine, index=False)
     conn.close()
+
+## snowflake
+"""
+export PREFECT__CONTEXT__SECRETS__SNOWFLAKE_USER=
+export PREFECT__CONTEXT__SECRETS__SNOWFLAKE_PASS=
+export PREFECT__CONTEXT__SECRETS__SNOWFLAKE_ACCOUNT_ID=
+export PREFECT__CONTEXT__SECRETS__SNOWFLAKE_DATABASE=
+export PREFECT__CONTEXT__SECRETS__SNOWFLAKE_WAREHOUSE=
+export PREFECT__CONTEXT__SECRETS__SNOWFLAKE_ROLE=
+"""
+
+
+def get_snowflake_connection_string(database: str) -> str:
+    user = Secret("SNOWFLAKE_USER").get()
+    pwd = Secret("SNOWFLAKE_PASS").get()
+    account_id = Secret("SNOWFLAKE_ACCOUNT_ID").get()
+    schema = "JAFFLE_SHOP"
+    warehouse = Secret("SNOWFLAKE_WAREHOUSE").get()
+    role = Secret("SNOWFLAKE_ROLE").get()
+    return f"snowflake://{user}:{pwd}@{account_id}/{database}/{schema}?warehouse={warehouse}&role={role}"
+
+
+def get_df_from_sql_query(table_or_query: str) -> pd.DataFrame:
+    db = get_snowflake_connection_string()
+    engine = create_engine(db)
+    return pd.read_sql(table_or_query, engine)
+
+
+def load_df_to_snowflake(df: pd.DataFrame, table_name: str, schema: str = "JAFFLE_SHOP") -> None:
+    conn_string = get_snowflake_connection_string()
+    db_engine = create_engine(conn_string)
+    conn = db_engine.connect()
+    # conn.execute(f"TRUNCATE TABLE {database}.{schema}.{table_name};")
+    df.to_sql(table_name, schema=schema, con=db_engine, if_exists="replace", index=False)
+    conn.close()
+
+
+@resource_manager
+class SnowflakeConnection:
+    def __init__(self):
+        self.database = Secret("SNOWFLAKE_DATABASE").get()
+
+    def setup(self):
+        db_conn_string = get_snowflake_connection_string(self.database)
+        db_engine = create_engine(db_conn_string)
+        return db_engine.connect()
+
+    @staticmethod
+    def cleanup(conn):
+        conn.close()
---
+++ flows_2/05_extract_load.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import prefect
+from prefect import task, Flow
+from prefect.executors import LocalDaskExecutor
+from prefect.run_configs import LocalRun
+from prefect.storage import GitHub
+
+from flow_utilities.db_utils import load_df_to_snowflake
+
+
+FLOW_NAME = "05_extract_load"
+STORAGE = GitHub(
+    repo="dfernandezcalle/flow-of-flows",
+    path=f"flows_2/{FLOW_NAME}.py",
+    access_token_secret="GITHUB_ACCESS_TOKEN",
+    ref="develop"
+)
+
+
+@task
+def extract_and_load(dataset: str) -> None:
+    logger = prefect.context.get("logger")
+    file = f"https://raw.githubusercontent.com/anna-geller/jaffle_shop/main/data/{dataset}.csv"
+    df = pd.read_csv(file)
+    load_df_to_snowflake(df, dataset)
+    logger.info("Dataset %s with %d rows loaded to DB", dataset, len(df))
+
+
+with Flow(
+    FLOW_NAME,
+    executor=LocalDaskExecutor(),
+    storage=STORAGE,
+    run_config=LocalRun(labels=["dev"]),
+) as flow:
+    datasets = ["raw_customers", "raw_orders", "raw_payments"]
+    dataframes = extract_and_load.map(datasets)
